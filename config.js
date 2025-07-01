6ca72e055a90d3af937e24b00514899b68f3815c
INCONNU BOY <inconnuboy39@gmail.com>
2025-07-01 12:22:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN || 'TON_TOKEN_TELEGRAM',
+  BOT_TOKEN: process.env.BOT_TOKEN || '7214172448:AAHGqSgaw-zGVPZWvl8msDOVDhln-9kExas',
   OWNER_ID: process.env.OWNER_ID || '7208911032',
   OWNER_NAME: 'INCONNU BOY',
   CHANNELS: ['@Sprem_toolkits', '@dybytm'],
