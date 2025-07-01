dd5bd12084a004e1fd23c2a9101dea7850bc678a
INCONNU BOY <inconnuboy39@gmail.com>
2025-07-01 12:16:11 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  BOT_TOKEN: process.env.BOT_TOKEN || 'TON_TOKEN_TELEGRAM',
+  OWNER_ID: process.env.OWNER_ID || '7208911032',
+  OWNER_NAME: 'INCONNU BOY',
+  CHANNELS: ['@Sprem_toolkits', '@dybytm'],
+  PAIRING_FOLDER: './lib2/pairing',
+  PREMIUM_FILE: './premium.json'
+};
